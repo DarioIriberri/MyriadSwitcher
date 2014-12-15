58c45665b758efbb7574b0e6ffe02c29907eff4f
Dario Iriberri <dario.iriberri@gmail.com>
2014-12-15 16:07:39 +0100

    Build batch adapted to new structure

--- src/win-build-cx_freeze_x84.bat
+++ src/win-build-cx_freeze_x84.bat
@@ -3,8 +3,9 @@ rmdir /s /q ..\build
 python setup_cx_freeze_x84.py build
 xcopy ..\img build\exe.win32-2.7\img /e /i /h
 xcopy ..\README build\exe.win32-2.7\README /e /i /h
-copy ..\config\defaults.conf build\exe.win32-2.7\myriadSwitcher.conf /y
-copy ..\config\activeConfig build\exe.win32-2.7 /y
+copy ..\config\*.* build\exe.win32-2.7\ /y
+xcopy ..\miners build\exe.win32-2.7\miners /e /i /h
+rem copy ..\config\activeConfig build\exe.win32-2.7 /y
 xcopy build ..\build /e /i /h
 rmdir /s /q build
 pause
